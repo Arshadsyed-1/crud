074ba6e3a2aaefd9be54ad15f2015123ad9a6b4b
arshad <syedarshad0756@gmail.com>
2026-05-24 21:06:41 +0530

    update code

--- app.py
+++ app.py
@@ -40,14 +40,14 @@ elif opt == "Update_employee":
     if btn:
         new_data = {"n": name, "e": email, "d": dept}
         response = requests.put(f"{server_location}/employees/{employee_id}", json=new_data)
-        st.success(response.json()["message"])
+        st.write(response.json())
 elif opt == "Delete_employee":
     st.header("Delete Employee")
     employee_id = st.number_input("Employee ID", min_value=1)
     btn = st.form_submit_button("Delete")
     if btn:
         response = requests.delete(f"{server_location}/employees/{employee_id}")
-        st.success(response.json()["message"])
+        st.write(response.json())
 
 
         
